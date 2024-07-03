a98575a9d83bfcee699e08359d629d54da6d7380
sedmedgh <sedmedgh@gmail.com>
2024-07-03 12:35:05 +0330

    fix: load image fail

--- src/dom/extra/dataurl.ts
+++ src/dom/extra/dataurl.ts
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import {getMimeType} from './mimes';
+
 function getContentFromDataUrl(dataURL: string) {
   return dataURL.split(/,/)[1]
 }
@@ -14,24 +17,50 @@ export async function fetchAsDataURL<T>(
   url: string,
   process: (data: {result: string; res: Response}) => T
 ): Promise<T> {
-  const res = await fetch(url)
-  if (res.status === 404) {
-    throw new Error(`Resource "${res.url}" not found`)
-  }
-  const blob = await res.blob()
-  return new Promise<T>((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onerror = reject
-    reader.onloadend = () => {
-      try {
-        resolve(process({res, result: reader.result as string}))
-      } catch (error) {
-        reject(error)
-      }
-    }
-
-    reader.readAsDataURL(blob)
+  const failPromise =(uri?:string)=>  new Promise<T>((resolve) => {
+    resolve((uri || url) as T)
   })
+  try {
+    const res = await axios({
+      url,
+      method: 'get',
+      // responseType: 'arraybuffer',
+      // headers:{
+      //   origin: window.location.origin,
+      //   host: window.location.host,
+      //   // mode: 'no-cors',
+      //   'Access-Control-Allow-Origin': '*',
+      //  'Content-Type':  '*',
+      //   Connection: 'keep-alive',
+      //   'Access-Control-Allow-Headers': '*',
+      //   'Access-Control-Allow-Credentials': 'true'
+      // },
+      // credentials: "same-origin",
+      // xhr: {
+      //   withCredentials: true
+      // },
+    })
+    if (res.status === 404) {
+      return failPromise('')
+    }
+    const blob = new Blob([res.data])
+    return new Promise<T>((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onerror = reject
+      reader.onloadend = () => {
+        try {
+          resolve(process({res, result: reader.result as string}))
+        } catch (error) {
+          reject(error)
+        }
+      }
+
+      reader.readAsDataURL(blob)
+    })
+  } catch (e) {
+    console.warn(e);
+    return failPromise()
+  }
 }
 
 const cache: {[url: string]: string} = {}
@@ -47,6 +76,31 @@ function getCacheKey(url: string, contentType: string | undefined) {
   return contentType ? `[${contentType}]${key}` : key
 }
 
+const canvasTry = async(url:string)=> {
+  const imgs =['png', 'jpg', 'jpeg', 'gif', 'tiff', 'svg', 'webp']
+  if (imgs.some(img => url.includes(img))){
+    return new Promise((resolve) => {
+      const canvas = document.createElement('canvas')
+      const ctx = canvas.getContext('2d')
+      const img = new Image()
+      img.crossOrigin = 'anonymous'
+      img.onload = function () {
+        if (!canvas || !ctx) {
+          resolve(url)
+        }
+        canvas.height = img.height
+        canvas.width = img.width
+        ctx?.drawImage(img, 0, 0)
+        resolve(canvas.toDataURL(getMimeType(img.src) || 'image/png'))
+      }
+      img.onerror = () => {
+        resolve(url)
+      }
+      img.src = url
+    })
+  }
+  return url
+}
 export async function resourceToDataURL(resourceUrl: string, contentType: string | undefined) {
   const cacheKey = getCacheKey(resourceUrl, contentType)
 
@@ -63,6 +117,8 @@ export async function resourceToDataURL(resourceUrl: string, contentType: string
       }
       return getContentFromDataUrl(result)
     })
+    if (content.includes('http'))
+      return await canvasTry(content)
     dataURL = makeDataUrl(content, contentType!)
   } catch (error) {
     dataURL = ''
--- src/dom/extra/embed-images.ts
+++ src/dom/extra/embed-images.ts
@@ -1,29 +1,29 @@
-import {embedResources} from './embed-resources'
-import {toArray, isInstanceOfElement} from './util'
-import {isDataUrl, resourceToDataURL} from './dataurl'
-import {getMimeType} from './mimes'
+import {embedResources} from './embed-resources';
+import {toArray, isInstanceOfElement} from './util';
+import {isDataUrl, resourceToDataURL} from './dataurl';
+import {getMimeType} from './mimes';
 
 async function embedProp(propName: string, node: HTMLElement) {
-  const propValue = node.style?.getPropertyValue(propName)
+  const propValue = node.style?.getPropertyValue(propName);
   if (propValue) {
-    const cssString = await embedResources(propValue, null)
-    node.style.setProperty(propName, cssString, node.style.getPropertyPriority(propName))
-    return true
+    const cssString = await embedResources(propValue, null);
+    node.style.setProperty(propName, cssString, node.style.getPropertyPriority(propName));
+    return true;
   }
-  return false
+  return false;
 }
 
 async function embedBackground<T extends HTMLElement>(clonedNode: T) {
   if (!(await embedProp('background', clonedNode))) {
-    await embedProp('background-image', clonedNode)
+    await embedProp('background-image', clonedNode);
   }
   if (!(await embedProp('mask', clonedNode))) {
-    await embedProp('mask-image', clonedNode)
+    await embedProp('mask-image', clonedNode);
   }
 }
 
 async function embedImageNode<T extends HTMLElement | SVGImageElement>(clonedNode: T) {
-  const isImageElement = isInstanceOfElement(clonedNode, HTMLImageElement)
+  const isImageElement = isInstanceOfElement(clonedNode, HTMLImageElement);
 
   if (
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -31,54 +31,71 @@ async function embedImageNode<T extends HTMLElement | SVGImageElement>(clonedNod
     !(isImageElement && !isDataUrl(clonedNode.src)) &&
     !(isInstanceOfElement(clonedNode, SVGImageElement) && !isDataUrl(clonedNode.href.baseVal))
   ) {
-    return
+    return;
   }
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const url = isImageElement ? clonedNode.src : clonedNode.href.baseVal
+  const url = isImageElement ? clonedNode.src : clonedNode.href.baseVal;
 
-  const dataURL = await resourceToDataURL(url, getMimeType(url))
+  const dataURL = await resourceToDataURL(url, getMimeType(url));
   await new Promise((resolve, reject) => {
-    clonedNode.onload = resolve
-    clonedNode.onerror = reject
+    clonedNode.onload = resolve;
+    clonedNode.onerror = (e) => {
+      console.log('e ===>', e);
+    };
 
-    const image = clonedNode as HTMLImageElement
+    const image = clonedNode as HTMLImageElement;
     if (image.decode) {
-      image.decode = resolve as any
+      image.decode = resolve as any;
     }
 
     if (image.loading === 'lazy') {
-      image.loading = 'eager'
+      image.loading = 'eager';
     }
 
     if (isImageElement) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      clonedNode.srcset = ''
+      clonedNode.srcset = '';
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      clonedNode.src = dataURL
+      clonedNode.src = dataURL;
     } else {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      clonedNode.href.baseVal = dataURL
+      clonedNode.href.baseVal = dataURL;
     }
-  })
+  });
 }
 
 async function embedChildren<T extends HTMLElement>(clonedNode: T) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const children = toArray<HTMLElement>(clonedNode.childNodes)
-  const deferreds = children.map((child) => embedImages(child))
-  await Promise.all(deferreds).then(() => clonedNode)
+  const children = toArray<HTMLElement>(clonedNode.childNodes);
+  const deferreds = children.map((child) => embedImages(child));
+  await Promise.all(deferreds).then(() => clonedNode);
 }
 
 export async function embedImages<T extends HTMLElement>(clonedNode: T) {
   if (isInstanceOfElement(clonedNode, Element)) {
-    await embedBackground(clonedNode)
-    await embedImageNode(clonedNode)
-    await embedChildren(clonedNode)
+    try{
+      await embedBackground(clonedNode);
+    }
+    catch (e) {
+      console.log('embedBackground ===>', e);
+    }
+    try{
+      await embedImageNode(clonedNode);
+    }
+    catch (e) {
+      console.log('embedImageNode ===>', e);
+    }
+    try{
+      await embedChildren(clonedNode);
+    }
+    catch (e) {
+      console.log('embedChildren ===>', e);
+    }
   }
 }
